23b3c13f92f1f3341b271836f7b013ca0ae6947d
Randel M <Randel.C.Moore@gmail.com>
2020-10-27 15:51:06 -0700

    add logic to give user the correct quiz answer
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Brian Sturgis <sturujisu@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
+  $("form#questions").submit(function(event){
+    event.preventDefault();
+  
+    let luckyArray = [];
+    let unLuckyArray = [];
+    $("input:checkbox[name=lucky]:checked").each(function(){
+      const luckyAnswers = $(this).val();
+      luckyArray.push(luckyAnswers);
+    });
 
+    $("input:checkbox[name=un-lucky]:checked").each(function(){
+      const unLuckyAnswers = $(this).val();
+      unLuckyArray.push(unLuckyAnswers);
+    });
+    
+    if (luckyArray.length > unLuckyArray.length) {
+      $("#goodfortune").show();
+      $("#badfortune").hide();
+      $("#neutral").hide();
+    }
+    else if (luckyArray.length < unLuckyArray.length) {
+      $("#badfortune").show();
+      $("#goodfortune").hide();
+      $("#neutral").hide();
+    }
+    else if (luckyArray.length = unLuckyArray.length) {
+      $("#neutral").show();
+      $("#goodfortune").hide();
+      $("#badfortune").hide();
+    }
+  });
 });
